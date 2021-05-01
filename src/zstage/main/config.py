85f678bdc35c6a66034f11eb02cbe54b5d0ed2fc
melvin sebastian <melvin97n@gmail.com>
2021-05-01 21:36:30 +0530

    updated datakit in python

--- src/zstage/main/config.py
+++ src/zstage/main/config.py
@@ -25,16 +25,16 @@ def setConfig(job):
     jobDesc = job
     if "Crime" in job:
         pastDate = date.DateRoutines().minusDays(1)
-        inputFile = f"{dirname(dirname(dirname(os.getcwd())))}\\Crime_Incidents.csv"
-        logFile = f"{dirname(dirname(dirname(os.getcwd())))}\\logs\\CompileCrime_" \
+        inputFile = f"Crime_Incidents.csv"
+        logFile = f"logs\\CompileCrime_" \
                   f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.txt"
         yyyymmdd = pastDate.strftime("%Y-%m-%d")
         inputURL = "https://data.buffalony.gov/resource/d6g9-xbgu.csv?$where=created_at%20%3E%20%27" + yyyymmdd + \
                    "T00:00:00%27"
 
     elif "Date" in job:
-        inputFile = f"{dirname(dirname(dirname(os.getcwd())))}\\Date.csv"
-        logFile = f"{dirname(dirname(dirname(os.getcwd())))}\\logs\\CompileDate_{datetime.now().strftime('%Y_%m_%d')}.txt"
+        inputFile = f"Date.csv"
+        logFile = f"logs\\CompileDate_{datetime.now().strftime('%Y_%m_%d')}.txt"
     return True
 
 
